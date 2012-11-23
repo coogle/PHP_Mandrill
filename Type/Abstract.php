70e1fa3f3a3efd2d2704c9957180090b66de731c
developer <developer@aquaripedia.(none)>
2012-11-23 13:55:24 -0500

    Forgot some stuff

--- Type/Abstract.php
+++ Type/Abstract.php
@@ -4,18 +4,68 @@ require_once 'Zend/Http/Client.php';
 
 abstract class Mandrill_Type_Abstract
 {
+	const BASE_API_URL = "https://mandrillapp.com/api/1.0";
+	
 	static protected $_httpClient;
+	
+	protected $_apiKey;
+	protected $_async;
+	
+	function __construct($apiKey, $async = true) {
+		$this->_apiKey = $apiKey;
+		$this->_async = (boolean)$async;
+	}
+	
+	public function getApiKey() {
+		return $this->_apiKey;
+	} 
+
+	public function isAsync() {
+		return (boolean)$this->_async;
+	}
 	
 	static public function setHttpClient(Zend_Http_Client $client) 
 	{
 		static::$_httpClient = $client;	
 	} 
 	
+	/**
+	 * Return an instance of the Zend_Http_Client
+	 * 
+	 * @return Zend_Http_Client
+	 */
 	static public  function getHttpClient() {
 		if(is_null(static::$_httpClient)) {
 			static::$_httpClient = new Zend_Http_Client();
+			static::$_httpClient->setMethod('POST');
 		}
 		
 		return static::$_httpClient;
+	}
+	
+	protected function _request($path, array $request) {
+		
+		$requestUri = static::BASE_API_URL . "$path";
+		
+		$client = static::getHttpClient();
+		
+		$client->setUri($requestUri);
+		
+		
+		$requestDataJson = json_encode($request);
+		
+		$client->setHeaders("Content-Type", "application/json");
+		
+		$client->setRawData($requestDataJson);
+		
+		$response = $client->request();
+
+		$answer = json_decode($response->getBody(), true);
+		
+		if($response->getStatus() != 200) {
+			throw new \Exception("Request Failed: {$answer['message']}");
+		}
+		
+		return true;
 	}
 }
--- Type/Messages.php
+++ Type/Messages.php
@@ -1,5 +1,8 @@
 <?php
 
+require_once 'Mandrill/Message/Type.php';
+require_once 'Mandrill/Type/Abstract.php';
+
 class Mandrill_Type_Messages extends Mandrill_Type_Abstract
 {
 	public function send() 
@@ -7,9 +10,28 @@ class Mandrill_Type_Messages extends Mandrill_Type_Abstract
 			
 	}
 	
-	public function sendTemplate()
+	public function sendTemplate($template, Mandrill_Message_Type $message, array $content = array())
 	{
+		$template_content = array();
 		
+		foreach($content as $key => $val) {
+			$template_content[] = array(
+				'name' => (string)$key,
+				'content' => (string)$val
+			);
+		}
+		
+		$message->setApiKey($this->getApiKey());
+		
+		$request = array(
+			'key' => $this->getApiKey(),
+			'template_name' => (string)$template,
+			'template_content' => $template_content,
+			'async' => $this->isAsync(),
+			'message' => $message->toArray()
+		);
+		
+		return $this->_request("/messages/send-template.json", $request);
 	}
 	
 	public function search()
